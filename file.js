441c1ba54e174a9abc4d917780087e8516da1a98
website <77297057+wwebsite@users.noreply.github.com>
2022-07-01 08:23:54 -0700

    Update file.js

--- file.js
+++ file.js
@@ -3,6 +3,12 @@ function addEmail(){
     newEmail = document.getElementById('add').value
     emailList.push(" " +  newEmail)
     document.getElementById('se').innerHTML = 'refresh emails'
+    // Store data
+    var someData = emailList
+    localStorage.setItem('myDataKey', someData);
+
+    // Get data
+    emailList.push(localStorage.getItem('myDataKey'))
 }
 
 function showEmails(){
