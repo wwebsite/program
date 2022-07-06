24c01f79d6343fad1089a7897e761f0d0f9a6bd7
website <77297057+wwebsite@users.noreply.github.com>
2022-07-06 08:43:58 -0700

    Update file.php

--- file.php
+++ file.php
@@ -1,19 +1,3 @@
-<form action="" method="post">
-    <input type="submit" value="Send details to embassy" />
-    <input type="hidden" name="button_pressed" value="1" />
-</form>
-
 <?php
-
-if(isset($_POST['button_pressed']))
-{
-    $to      = 'ivanchernichenko@gmail.com';
-    $subject = 'lets see if this woks';
-    $message = message;
-
-    mail($to, $sujbect, $message);
-
-    echo 'Email Sent.';
-}
-
+mail('themak722@gmail.com', 'test subject', 'hello','From: themak7222@gmail.com');
 ?>
